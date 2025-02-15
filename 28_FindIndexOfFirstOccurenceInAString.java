1a867ce070eaf495a30ce288c9d2663f07d460d9
Mohd Aman <118279443+amann-273@users.noreply.github.com>
2025-02-15 22:50:44 +0530

    Initial commit

---
+++ 28_FindIndexOfFirstOccurenceInAString.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public int strStr(String haystack, String needle) 
+    {
+        // Iterate through the haystack with a sliding window of size equal to needle length
+        for(int i = 0, j = needle.length(); j <= haystack.length(); i++, j++) 
+        {
+            // Extract the substring from haystack with the same length as needle
+            if(haystack.substring(i, j).equals(needle)) 
+            {
+                // If the extracted substring matches the needle, return the starting index
+                return i;
+            }
+        }
+        return -1;
+    }
+}
